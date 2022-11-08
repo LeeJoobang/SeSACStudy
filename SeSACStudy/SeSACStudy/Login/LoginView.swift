26ba715b136f0a90dbefce044f7fce6c57cd1d94
Joobang <hii507@gmail.com>
2022-11-08 23:53:33 +0900

    [add] loginview + textfield, label 표시

---
+++ SeSACStudy/SeSACStudy/Login/LoginView.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+
+class LoginView: BaseView{
+    
+    let numberTextLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let numberTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    let certificationButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func configure() {
+        [numberTextLabel, numberTextField, certificationButton].forEach {
+            self.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        numberTextLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(125)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(64)
+        }
+        
+        numberTextField.snp.makeConstraints { make in
+            make.top.equalTo(numberTextLabel.snp.bottom).offset(64)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(48)
+        }
+        
+        certificationButton.snp.makeConstraints { make in
+            make.top.equalTo(numberTextField.snp.bottom).offset(72)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(48)
+        }
+    }
+}
--- SeSACStudy/SeSACStudy/Login/LoginViewController.swift
+++ SeSACStudy/SeSACStudy/Login/LoginViewController.swift
@@ -1,9 +1,32 @@
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: BaseViewController {
 
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        self.view = loginView
+        loginView.backgroundColor = .white
+    }
+    
+    override func configure() {
+        loginView.numberTextLabel.numberOfLines = 0
+        loginView.numberTextLabel.font = loginView.numberTextLabel.font.withSize(20)
+        loginView.numberTextLabel.text = "새싹 서비스 이용을 위해 \n휴대폰 번호를 입력해 주세요"
+        loginView.numberTextLabel.setLineSpacing(spacing: 15.0)
+        loginView.numberTextLabel.textAlignment = .center
+        
+        loginView.numberTextField.placeholder = "휴대폰 번호(-없이 숫자만 입력)"
+    }
+}
+extension UILabel {
+    func setLineSpacing(spacing: CGFloat){
+        guard let text = text else { return }
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        attributeString.addAttribute(.paragraphStyle, value: style, range: NSRange(location: 0, length: attributeString.length))
+        attributedText = attributeString
     }
 }
